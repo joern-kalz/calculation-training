f584d35e89454258b05182ccac9e7b1460c697cc
Jörn Kalz <49426396+joern-kalz@users.noreply.github.com>
2026-05-02 10:24:29 +0200

    fix: Base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/calculation-training',
+  assetPrefix: '/calculation-training/',
 };
 
 export default nextConfig;
